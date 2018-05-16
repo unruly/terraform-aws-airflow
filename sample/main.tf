40a560ba0dcd4605ac2c3d83db21db82fc898151
Alex Wilson <a.wilson@alumni.warwick.ac.uk>
2018-05-16 16:44:24 +0000

    fix(sample): Enable DNS hostnames in the sample
    * The output from the airflow module is empty if the VPC doesn't have public dns enabled

--- sample/main.tf
+++ sample/main.tf
@@ -13,6 +13,8 @@ module "vpc" {
   azs             = ["eu-west-1a",     "eu-west-1b",     "eu-west-1c"    ]
   private_subnets = ["10.10.1.0/24",   "10.10.2.0/24",   "10.10.3.0/24"  ]
   public_subnets  = ["10.10.101.0/24", "10.10.102.0/24", "10.10.103.0/24"]
+
+  enable_dns_hostnames = true
 }
 
 module "airflow" {
